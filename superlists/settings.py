7a7e15dbce00f7d4f1a7384a03551fd3d55f4e7c
Mauricio <maugravena@gmail.com>
2020-03-17 08:57:29 -0300

    fix installed app auth

--- superlists/settings.py
+++ superlists/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.auth',
+    # 'django.contrib.admin',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
